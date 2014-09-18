75ff31c71f627bbd70c18be249e75385aae7faaf
jack <jack@bnrcmatoMacBook-Pro.local>
2014-09-18 19:25:21 +0800

    Initial Commit

---
+++ sensoriosclient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sensoriosclient
+//
+//  Created by jack on 14-9-18.
+//  Copyright (c) 2014年 bnrc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ sensoriosclient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  sensoriosclient
+//
+//  Created by jack on 14-9-18.
+//  Copyright (c) 2014年 bnrc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
